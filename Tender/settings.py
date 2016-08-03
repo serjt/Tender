12d771a256c7202f9cb67bbd65e855d8654bc896
serjt <tanaki.93@gmail.com>
2016-08-03 17:39:00 +0600

    NEW commit

--- Tender/settings.py
+++ Tender/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9_^99om!qw@m(@eg$lgka62137yefda7v(u(y)_woxr26xcij6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
