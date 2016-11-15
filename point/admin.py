5af28d59d42d39b09bc6091a53f4ecf3956d5695
serjt <tanaki.93@gmail.com>
2016-11-16 00:00:08 +0600

    admin, api and models changed

--- point/admin.py
+++ point/admin.py
@@ -8,7 +8,6 @@ from django.contrib.auth.models import Group,User
 admin.site.unregister(Group)
 admin.site.unregister(User)
 admin.site.unregister(ApiKey)
-admin.site.unregister(ApiAccess)
 # Register your models here.
 
 
