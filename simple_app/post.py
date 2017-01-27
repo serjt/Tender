63f63b6a741eb818e1dc0207d3eb99277784d45a
serjt <tanaki.93@gmail.com>
2017-01-27 22:12:16 +0600

    news kg edited

--- simple_app/post.py
+++ simple_app/post.py
@@ -1,6 +1,9 @@
 # coding=utf-8
 import requests
 from bs4 import BeautifulSoup
+import json
+
+from django.http import HttpResponse
 
 
 def find_data(fio, d, m, y):
@@ -9,14 +12,52 @@ def find_data(fio, d, m, y):
     soup = BeautifulSoup(response.content, "lxml")
     strong = soup.find_all('strong')
     str1 = str(strong[0])
-    return str1[8:-9]
-# fio = "asdf"
-# d = "01"
-# m = "01"
-# y = "2016"
+    str2 = str1[8:-9]
+    if str2 == 'Указанная фамилия в «Чёрном списке» не найдена!':
+        return str2
+    else:
+        c = []
+        for a in soup.find_all('a', href=True):
+            url = a['href']
+            if url[0:11] == '/blacklist/':
+                res = requests.get(url='http://www.ssm.gov.kg' + url)
+                soup1 = BeautifulSoup(res.content, "lxml")
+                p = soup1.find_all('p')
+                p1 = str(p[0])
+                context = {
+                    'name': a.text,
+                    'nakaz': p1[3:-4]
+                }
+                c.append(context)
+        return c
+#
+# fio = "Кенешов Мирлан Абдыразакович"
+# d = "19"
+# m = "10"
+# y = "1984"
 # response = requests.post(url='http://www.ssm.gov.kg/blacklist/',
 #                          data=dict(fio=fio, d=d, m=m, y=y))
 # soup = BeautifulSoup(response.content, "lxml")
 # strong = soup.find_all('strong')
 # str1 = str(strong[0])
-# print str1[8:-9]
+# str2 = str1[8:-9]
+# if str2 == 'Указанная фамилия в «Чёрном списке» не найдена!':
+#     print str2
+# else:
+#     c = []
+#     for a in soup.find_all('a', href=True):
+#         url = a['href']
+#         if url[0:11] == '/blacklist/':
+#             res = requests.get(url='http://www.ssm.gov.kg' + url)
+#             soup1 = BeautifulSoup(res.content, "lxml")
+#             p = soup1.find_all('p')
+#             p1 = str(p[0])
+#             context = {
+#                 'name': a.text,
+#                 'nakaz': p1[3:-4]
+#             }
+#             c.append(context)
+#     l = {
+#         'object': c,
+#     }
+#     print str(json.dumps(l, separators=(',', ':')))
